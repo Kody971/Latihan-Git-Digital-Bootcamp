dc625533e917c78f63765b0b0fde262aa1380c09
ideki11 <ideki0588@gmail.com>
2022-10-12 06:35:10 +0700

    update showInformation()

--- src/Customer.java
+++ src/Customer.java
@@ -39,5 +39,8 @@ public class Customer {
 		this.age = age;
 	}
 	
-	
+	public void showInformation() {
+	    System.out.println("Name : " + getName());
+	    System.out.println("Address : " + getAddress() );
+	}
 }
